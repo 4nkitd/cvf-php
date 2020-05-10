209b98167d21bb2d4096d1089dc6f69f38b6e81a
Ankit <4nkitd@gmail.com>
2020-05-10 19:33:25 +0530

    response function added

--- core/helpers.php
+++ core/helpers.php
@@ -81,3 +81,70 @@ function js_write_email($email)
 			<noscript>Please enable JavaScript to view emdails</noscript>
 		';
 }
+
+function response(array $response = [],int $code = 200,string $type = 'json'){
+
+    $statusCodes = [
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Found',  // 1.1
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        307 => 'Temporary Redirect',
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Timeout',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Request Entity Too Large',
+        414 => 'Request-URI Too Long',
+        415 => 'Unsupported Media Type',
+        416 => 'Requested Range Not Satisfiable',
+        417 => 'Expectation Failed',
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Timeout',
+        505 => 'HTTP Version Not Supported',
+        509 => 'Bandwidth Limit Exceeded'
+    ];
+
+    $status = $statusCodes[$code];
+
+    header("HTTP/1.1 ".$code." ".$status);
+
+    if($type == 'json'){
+        header('Content-Type: application/json');
+        $response = json_encode($response,true);
+    } elseif ($type == 'xml') {
+        header('Content-type: application/xml');
+        $response = simplexml_load_string($response);
+    }
+
+    $length = strlen($response);
+
+    header('Content-Length: '.$length);
+
+    echo $response;
+
+    exit();
+}
